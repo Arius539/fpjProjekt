17de035ace0b7bd8d2501d45efac53043e01aa5b
colin <colingkirchner@gmail.com>
2025-12-18 22:57:59 +0100

    TransactionTest complete

--- src/test/java/org/fpj/DBTests/payments/TransactionTest.java
+++ src/test/java/org/fpj/DBTests/payments/TransactionTest.java
@@ -1,5 +1,6 @@
 package org.fpj.DBTests.payments;
 
+import org.fpj.exceptions.DataNotPresentException;
 import org.fpj.payments.application.TransactionService;
 import org.fpj.payments.domain.TransactionLite;
 import org.fpj.payments.domain.TransactionRepository;
@@ -20,6 +21,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @Testcontainers
 @SpringBootTest
@@ -59,10 +61,11 @@ public class TransactionTest {
 
     @BeforeEach
     public void setUp(){
+        txRepo.deleteAll();
         userRepo.deleteAll();
 
-        userRepo.saveAndFlush(currentUser);
-        userRepo.saveAndFlush(user2);
+        userRepo.save(currentUser);
+        userRepo.save(user2);
 
         userId1 = currentUser.getId();
         userId2 = user2.getId();
@@ -119,6 +122,23 @@ public class TransactionTest {
         BigDecimal expected = BigDecimal.valueOf(20);
 
         assertEquals(0, balanceAfterPayout.compareTo(expected));
+    }
+
+    @Test
+    public void testTransferNoUserFound(){
+        userRepo.delete(user2);
+
+        BigDecimal amount = BigDecimal.valueOf(30);
+        BigDecimal amount2 = BigDecimal.valueOf(10);
+        TransactionLite transactionLite1 = new TransactionLite(amount, TransactionType.EINZAHLUNG, null, USERNAME1, SUBJECT);
+        TransactionLite transactionLite2 = new TransactionLite(amount2, TransactionType.UEBERWEISUNG, USERNAME1, USERNAME2, SUBJECT);
+
+        transactionService.sendTransfers(transactionLite1, currentUser);
+        BigDecimal balanceBeforeTransfer = txRepo.computeBalance(userId1);
+        assertThrows(DataNotPresentException.class, () -> transactionService.sendTransfers(transactionLite2, currentUser));
+        BigDecimal balanceAfterFailedTransfer = txRepo.computeBalance(userId1);
+
+        assertEquals(0, balanceAfterFailedTransfer.compareTo(balanceBeforeTransfer));
     }
 
 }
