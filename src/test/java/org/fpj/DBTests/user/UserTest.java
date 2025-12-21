e19bb1337ad41516fe226447b7a68ef196e93977
colin <colingkirchner@gmail.com>
2025-12-21 19:25:44 +0100

    UserTest

--- src/test/java/org/fpj/DBTests/user/UserTest.java
+++ src/test/java/org/fpj/DBTests/user/UserTest.java
@@ -18,7 +18,6 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import java.time.Instant;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
