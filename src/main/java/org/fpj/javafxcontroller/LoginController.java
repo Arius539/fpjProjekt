0f29305dd44f2d5ffad52a2917454228898222a2
colin <colingkirchner@gmail.com>
2025-12-22 17:25:58 +0100

    Testfehler beseitigt

--- src/main/java/org/fpj/javafxcontroller/LoginController.java
+++ src/main/java/org/fpj/javafxcontroller/LoginController.java
@@ -13,7 +13,6 @@ import org.fpj.users.application.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
